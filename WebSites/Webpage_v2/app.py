1fac2ae40c7732b62799bdef5bfdfe1891a336a9
Sean Chuang <sean_chuang@berkeley.edu>
2020-04-16 22:31:04 -0700

    read csv and display books in result table if user lack of python or R skills

--- WebSites/Webpage_v2/app.py
+++ WebSites/Webpage_v2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+import pandas as pd
 
 # csv = readCSV('./...csv')
 # csvData
@@ -23,22 +24,32 @@ def handleReq():
 	age = request.form['age']
 	hasWorked = request.form['hasWorked']
 	yrOfExp = request.form['yrOfExp']
-	# skillSets = request.form.getlist("skillSets")
+	skillSets = request.form.getlist("skillSets")
 	print(request.form)
 	print(name, age)
 
-	## To-do:
-	
+	# Read book directory csv and display it if user lack reletive skills
+	book_list = list()
+	if 'Python' not in skillSets:
+		filename = 'Book_Directory_Python.csv'
+		data_book_Python = pd.read_csv(filename, header=0)
+		book_list += list(data_book_Python.values)
+	if 'R' not in skillSets:
+		filename = 'Book_Directory_R.csv'
+		data_book_R = pd.read_csv(filename, header=0)
+		book_list += list(data_book_R.values)
+
 	# check if every variable is valid
-	# if (error) 
+	# if (error)
 
 
 	# major = findMajor(request.form['major'])
 	# pass variables to Model
 
-	return render_template("Result.html", name=name, age=age, hasWorked = hasWorked, yrOfExp = yrOfExp) #result=modelResult, )
+	return render_template("Result.html", name=name, age=age, hasWorked = hasWorked,
+	yrOfExp = yrOfExp, skillSets = skillSets, book_list = book_list) #result=modelResult, )
 
-# Get the local data passed to HTML 
+# Get the local data passed to HTML
 @app.route('/post/<int:book_id>')
 def show_post(book_id):
 	return "the book id is %s" % book_id
@@ -51,7 +62,7 @@ def Result(age):
     # 	age = request.form['age']
     # 	return render_template('Result.html', age=age)
 
-    # else:	
+    # else:
     # 	return render_template('JobHunting.html')
 
 if __name__ == "__main__":
